e23e116d41c776097c69268c5962cf76a2ec7c86
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-19 22:26:11 +0000

    :sparkles: feat: componentize the URL input button

---
+++ src/components/input/inputURLButton.jsx
@@ -0,0 +1,6 @@
+export const InputURLButton = () => {
+    return (
+        <button className="w-36 p-2 rounded-md border border-blue-700 bg-blue-700 text-white text-md transition ease-linear
+        hover:border-blue-500 hover:bg-zinc-100 hover:text-blue-700">Shorten URL</button>
+    );
+};
