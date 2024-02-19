9a7be1a039ae281e9003bbe8a533989daa2a4578
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-19 21:36:23 +0000

    :lipstick: style: style the component and its elements (title, links and button)

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,13 +2,18 @@ import { Link } from "react-router-dom";
 
 export const Header = () => {
     return (
-        <header className="p-5 flex items-center justify-between">
-            <h1>URL Shortener</h1>
-            <nav>
-               <Link to={'/'}>Home</Link> 
-               <Link to={'/about'}>About</Link>  
+        <header className="flex items-center justify-between p-5">
+            <Link to={'/'}>
+                <h1 className="text-3xl font-bold tracking-wide text-blue-500">React URL</h1>
+            </Link>
+          <nav>
+               <Link className="p-2 ml-2 text-lg transition ease-linear border-b-2 border-white 
+               hover:text-blue-500 hover:border-blue-500"to={'/'}>Home</Link> 
+               <Link className="p-2 ml-3 text-lg transition ease-linear border-b-2 border-white 
+               hover:text-blue-500 hover:border-blue-500"to={'/about'}>About</Link>  
                <a href="https://github.com/OVasconceloss/url-shortener" target="_blank">
-                    <button>
+                    <button className="w-24 p-1 ml-8 rounded-lg border-2 bg-blue-500 text-white text-lg transition ease-linear 
+                    hover:bg-white hover:text-blue-500 hover:border-blue-500">
                         <i className="fa-brands fa-github"></i>
                     </button>
                 </a> 
