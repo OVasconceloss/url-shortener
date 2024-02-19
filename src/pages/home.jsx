3f20ad85c0749de891e54f480b2d42c4f4c217d4
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-19 22:22:09 +0000

    :hammer: refactor: change the flex property of the text and input section to flex-col

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <Header />
         <hr className="mt-5 border-zinc-200" />
         <main className="p-5 m-10 flex items-center justify-center">
-            <section className="p-5 flex items-center justify-center">
+            <section className="p-5 flex-col items-center justify-center">
                 <div className="flex flex-col items-center justify-center">
                     <h2 className="py-2 text-4xl text-blue-500 tracking-wide uper">Free URL Shortener</h2>
                     <p className="pb-2 text-lg">Minimize Links, Maximize Convenience: Your Ultimate URL Shortening Solution</p>
