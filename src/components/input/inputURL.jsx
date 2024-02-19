c98ce00d04126fa2c4992566fc6e12b8580f62c0
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-19 22:26:29 +0000

    :sparkles: feat: import input URL button component

--- src/components/input/inputURL.jsx
+++ src/components/input/inputURL.jsx
@@ -1,3 +1,5 @@
+import { InputURLButton } from "./inputURLButton";
+
 export const InputURL = () => {
     return (
         <div className="flex items-center p-5 my-2 bg-blue-500 rounded-lg space-x-3">
@@ -8,8 +10,7 @@ export const InputURL = () => {
                 className="p-2 w-full rounded-md outline-none placeholder:text-zinc-400 border border-blue-500 transition ease-linear
                 focus:border-blue-700 focus:placeholder:text-blue-400"
             />
-            <button className="w-36 p-2 rounded-md border border-blue-700 bg-blue-700 text-white text-md transition ease-linear
-            hover:border-blue-500 hover:bg-zinc-100 hover:text-blue-700">Shorten URL</button>
+            <InputURLButton />  
         </div>
     );
 };
