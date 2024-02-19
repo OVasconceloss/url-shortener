c6a7b4da2f8b2d7023cc1353188cab1525b91d23
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-19 20:46:41 +0000

    :sparkles: feat: create the file for the routes

---
+++ src/router.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+export const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route />
+            </Routes>
+        </BrowserRouter>
+    );
+};
