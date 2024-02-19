964a6565e5ac6a33c5e764be37365cf192bb5979
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-19 22:42:12 +0000

    :lipstick: style: stylize the title of the main to be bold

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         <main className="p-5 m-10 flex flex-col items-center justify-center">
             <section className="p-5 flex-col items-center justify-center">
                 <div className="flex flex-col items-center justify-center">
-                    <h2 className="py-2 text-4xl text-blue-500 tracking-wide uper">Free URL Shortener</h2>
+                    <h2 className="py-2 text-4xl font-bold text-blue-500 tracking-wide uper">Free URL Shortener</h2>
                     <p className="pb-2 text-lg">Minimize Links, Maximize Convenience: Your Ultimate URL Shortening Solution</p>
                 </div>
                 <div>
