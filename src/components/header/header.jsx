b17afd77855e157588bacfae8f06f0ba91157fc7
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-20 08:50:20 +0000

    :sparkles: feat: create dark mode button

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -12,11 +12,15 @@ export const Header = () => {
                <Link className="p-2 ml-3 text-lg transition ease-linear border-b-2 border-white 
                hover:text-blue-500 hover:border-blue-500"to={'/about'}>About</Link>  
                <a href="https://github.com/OVasconceloss/url-shortener" target="_blank">
-                    <button className="w-24 p-1 ml-8 rounded-lg border-2 bg-blue-500 text-white text-lg transition ease-linear 
+                    <button className="w-20 p-1 ml-8 rounded-lg border-2 bg-blue-500 text-white text-lg transition ease-linear 
                     hover:bg-white hover:text-blue-500 hover:border-blue-500">
                         <i className="fa-brands fa-github"></i>
                     </button>
-                </a> 
+                </a>
+                <button className="w-20 rounded-lg p-1 ml-2 border-2 bg-transparent text-blue-500 text-lg transition ease-linear 
+                hover:bg-blue-500 hover:text-white hover:border-blue-500">
+                    <i class="fa-solid fa-moon"></i>
+                </button>
             </nav>
         </header>
     );
