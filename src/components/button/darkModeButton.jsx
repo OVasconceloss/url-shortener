060766712bb7e832552508d4a75ad9658cefdb17
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-20 09:11:45 +0000

    :sparkles: feat: create the function to change the user's theme

--- src/components/button/darkModeButton.jsx
+++ src/components/button/darkModeButton.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+import { useDarkSide } from "../../hooks/userDarkSide";
+
 export const DarkModeButton = () => {
+    const [colorTheme, setTheme] = useDarkSide();
+    const [darkSide, setDarkSide] = useState(colorTheme === 'dark');
+
+    const toggleDarkMode = () => {
+        const newTheme = darkSide ? 'light' : 'dark';
+        setTheme(newTheme);
+        setDarkSide(!darkSide);
+    }
+
     return (
         <button className="w-20 rounded-lg p-1 ml-2 border-2 bg-transparent text-blue-500 text-lg transition ease-linear 
-        hover:bg-blue-500 hover:text-white hover:border-blue-500">
-            <i class="fa-solid fa-moon"></i>
+        hover:bg-blue-500 hover:text-white hover:border-blue-500" onClick={toggleDarkMode}>
+            <i className={`fa-solid fa-${darkSide ? 'sun' : 'moon'}`}></i>
         </button>
     );
 };
