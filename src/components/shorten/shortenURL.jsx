5858bc4395dd1ad727a63ffe3dd7b091b931cacd
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-21 09:34:31 +0000

    :sparkles: feat: create the function to copy the link to the clipboard (and its style)

--- src/components/shorten/shortenURL.jsx
+++ src/components/shorten/shortenURL.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
 export const ShortenURLResult = ({urlShortened}) => {
+    const [copyURL, setCopyURL] = useState(false);
+
+    const handleCopyURL = (urlCopy) => {
+        navigator.clipboard.writeText(urlCopy);
+        setCopyURL(true);
+
+        setTimeout(() => {
+            setCopyURL(false);
+        }, 2000);
+    }
+
     return (
         <>
         {
@@ -9,9 +22,16 @@ export const ShortenURLResult = ({urlShortened}) => {
                 <a href={`${urlShortened.link}`}>
                     <h3 className="text-lg text-blue-600 transition ease-linear hover:text-gray-500 dark:hover:text-blue-500 dark:text-white">{urlShortened.link}</h3>
                 </a>
-                <button className="p-2 text-md bg-blue-500 rounded-md text-white border border-white transition ease-linear
-                hover:border-blue-500 hover:bg-zinc-100 hover:text-blue-500
-                dark:border-slate-600 dark:hover:bg-slate-600 dark:hover:border-white dark:hover:text-white">Copy URL to Clipboard</button>
+                <button
+                    data-success={copyURL} 
+                    className="p-2 text-md w-48 bg-blue-500 rounded-md text-white border border-white transition ease-linear
+                    hover:border-blue-500 hover:bg-zinc-100 hover:text-blue-500
+                    dark:border-slate-600 dark:hover:bg-slate-600 dark:hover:border-white dark:hover:text-white
+                    data-[success=true]:bg-emerald-500 data-[success=true]:hover:bg-emerald-500 
+                    data-[success=true]:dark:hover:bg-emerald-500 data-[success=true]:hover:border-emerald-500 data-[success=true]:dark:hover:border-emerald-500"
+                    onClick={handleCopyURL}>
+                        {copyURL ? <i class="fa-regular fa-circle-check"></i> : 'Copy URL to Clipboard'}
+                    </button>
             </div>
             :
             <div></div> 
