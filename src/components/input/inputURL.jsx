fb2800c4ee0407636522ee33cbf8a1db54eaebcc
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-21 08:31:49 +0000

    :sparkles: feat: receive the user's URL input

--- src/components/input/inputURL.jsx
+++ src/components/input/inputURL.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { InputURLButton } from "./inputURLButton";
 
 export const InputURL = () => {
+    const [userURL, setUserURL] = useState("");
+
     return (
         <div className="flex items-center p-5 my-2 bg-blue-500 rounded-lg space-x-3 dark:bg-slate-700">
             <input 
@@ -10,8 +13,9 @@ export const InputURL = () => {
                 className="p-2 w-full rounded-md outline-none placeholder:text-zinc-400 border border-blue-500 transition ease-linear
                 focus:border-blue-700 focus:placeholder:text-blue-400 
                 dark:border-slate-600 dark:bg-slate-800 dark:focus:border-blue-500"
+                onChange={(inputText) => setUserURL(inputText.target.value)}
             />
-            <InputURLButton />  
+            <InputURLButton userURL={userURL} setUserURL={setUserURL} />  
         </div>
     );
 };
