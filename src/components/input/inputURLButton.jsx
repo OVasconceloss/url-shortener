0605683e903b2835869336285351ed38bca5325b
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-21 08:49:29 +0000

    :sparkles: feat: create the function to call the bitly API (and shorten the link)

--- src/components/input/inputURLButton.jsx
+++ src/components/input/inputURLButton.jsx
@@ -1,6 +1,25 @@
-export const InputURLButton = () => {
+import { useState } from "react";
+import shortenURL from "../../api/shortenAPI";
+
+export const InputURLButton = ({userURL, setUserURL}) => {
+    const [dataURL, setDataURL] = useState("");
+
+    const handleShortenURL = async () => {
+        try {
+            const responseData = await shortenURL.post('/shorten', {long_url: userURL});
+            setDataURL(responseData.data);
+        } catch (requestError) {
+            console.log(`Error: ${requestError}`);
+        }
+
+        setUserURL("");
+    }
     return (
-        <button className="w-36 p-2 rounded-md border border-blue-700 bg-blue-700 text-white text-md transition ease-linear
-        hover:border-blue-500 hover:bg-zinc-100 hover:text-blue-700 dark:hover:bg-slate-700 dark:hover:text-white dark:hover:border-white">Shorten URL</button>
+        <button 
+            className="w-36 p-2 rounded-md border border-blue-700 bg-blue-700 text-white text-md transition ease-linear
+            hover:border-blue-500 hover:bg-zinc-100 hover:text-blue-700 dark:hover:bg-slate-700 dark:hover:text-white dark:hover:border-white"
+            onClick={handleShortenURL}>
+                Shorten URL
+        </button>
     );
 };
