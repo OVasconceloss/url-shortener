59676f23f0d49f462f585964dba2a3aec0a0019d
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-21 08:32:36 +0000

    :bug: fix: correct the use of axios (from .post to .create)

--- src/api/shortenAPI.jsx
+++ src/api/shortenAPI.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiURL = 'https://api-ssl.bitly.com/v4/shorten';
 const accessToken = import.meta.env.VITE_BITLY_TOKEN_ACCESS;
 
-const shortenURL = axios.post({
+const shortenURL = axios.create({
     baseURL: apiURL,
     headers: {
         Authorization: `Bearer ${accessToken}`
