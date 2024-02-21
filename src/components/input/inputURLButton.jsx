fcbb5e02ccaf0e50d4184d272dc24aee60d843b7
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-21 08:58:14 +0000

    :sparkles: feat: use the callback function to return the shortened URL data to the parent component

--- src/components/input/inputURLButton.jsx
+++ src/components/input/inputURLButton.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
 import shortenURL from "../../api/shortenAPI";
 
-export const InputURLButton = ({userURL, setUserURL}) => {
+export const InputURLButton = ({userURL, setUserURL, setShortenURL}) => {
     const [dataURL, setDataURL] = useState("");
 
     const handleShortenURL = async () => {
         try {
             const responseData = await shortenURL.post('/shorten', {long_url: userURL});
+
             setDataURL(responseData.data);
+            setShortenURL(responseData.data);
         } catch (requestError) {
             console.log(`Error: ${requestError}`);
         }
 
         setUserURL("");
     }
+
     return (
         <button 
             className="w-36 p-2 rounded-md border border-blue-700 bg-blue-700 text-white text-md transition ease-linear
