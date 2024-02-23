1191ec86bc87a4c7177bfda37a2905b77d0226f3
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-23 16:58:09 +0000

    :boom: feat: show a message to verify that the environment variable is being accessed

--- src/api/shortenAPI.jsx
+++ src/api/shortenAPI.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const apiURL = 'https://api-ssl.bitly.com/v4/';
 const accessToken = import.meta.env.VITE_BITLY_TOKEN_ACCESS;
+console.log(accessToken)
 
 const shortenURL = axios.create({
     baseURL: apiURL,
