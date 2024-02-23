e058c49d750a8fa0c1c6a6b6d1c01efd911857db
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-23 16:04:23 +0000

    :sparkles: feat: create a component for loading

---
+++ src/components/loading/loadingAnimation.jsx
@@ -0,0 +1,7 @@
+export const LoadingAnimation = () => {
+    return (
+        <div className="w-[38rem] h-2 bg-gray-200 rounded-sm overflow-hidden">
+            <div className="h-full bg-emerald-500 animate-progress"></div>
+        </div>
+    );
+};
