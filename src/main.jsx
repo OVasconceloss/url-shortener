ef0eda6afa19e177a966873bcf79327ff8e515f1
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-23 09:40:01 +0000

    :sparkles: feat: import style file for scrollbar in main

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './styles/index.css';
+import './styles/scrollbar.css';
 import { Router } from './router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<Router />);
