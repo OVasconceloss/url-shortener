c6835a651c256978b56d574615c9e8d3c99644b1
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-23 16:05:10 +0000

    :wrench: feat: set the loading animation settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@
     darkMode: 'class',
     content: ["./src/**/*.{html,js,jsx}"],
     theme: {
-      extend: {},
+      extend: {
+        animation: {
+          progress: "progress 2s infinite",
+        },
+        keyframes: {
+          progress: {
+            "0%": { width: "0%" },
+            "100%": { width: "100%" },
+          }
+        }
+      },
     },
     plugins: [],
   }
