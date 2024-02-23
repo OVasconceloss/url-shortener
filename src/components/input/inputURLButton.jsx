0b64f930b631db7977874e5152d5247865cf5223
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-23 15:04:34 +0000

    :sparkles: feat: create a function to check if the URL is valid

--- src/components/input/inputURLButton.jsx
+++ src/components/input/inputURLButton.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import shortenURL from "../../api/shortenAPI";
 
-export const InputURLButton = ({userURL, setUserURL, setShortenURL}) => {
+export const InputURLButton = ({userURL, setUserURL, setShortenURL, setInputError}) => {
     const [dataURL, setDataURL] = useState("");
+    const [validURL, setValidURL] = useState();
+
+
+    const handleleValidateURL = (URL) => {
+        const regexURL = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/;
+        return regexURL.test(URL);
+    }
 
     const handleShortenURL = async () => {
         try {
@@ -11,17 +18,35 @@ export const InputURLButton = ({userURL, setUserURL, setShortenURL}) => {
             setDataURL(responseData.data);
             setShortenURL(responseData.data);
         } catch (requestError) {
-            console.log(`Error: ${requestError}`);
+            console.log(requestError);
         }
 
         setUserURL("");
+    }
+
+    const handleVerifyURL = () => {
+        if (userURL == '') {
+            setInputError(true);
+
+            setTimeout(() => {
+                setInputError(false);
+            }, 3000);
+        } else if (!handleleValidateURL(userURL)) {
+            setInputError(true);
+
+            setTimeout(() => {
+                setInputError(false);
+            }, 3000);
+        } else {
+            handleShortenURL();
+        }
     }
 
     return (
         <button 
             className="w-36 p-2 rounded-md border border-blue-700 bg-blue-700 text-white text-md transition ease-linear
             hover:border-blue-500 hover:bg-zinc-100 hover:text-blue-700 dark:hover:bg-slate-700 dark:hover:text-white dark:hover:border-white"
-            onClick={handleShortenURL}>
+            onClick={handleVerifyURL}>
                 Shorten URL
         </button>
     );
